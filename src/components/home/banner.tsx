d955eadc535adcd4cf452d570627d1dab1d99c24
enskerti <enskerti@gmail.com>
2025-03-06 17:21:53 +0300

    banner updated

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -7,39 +7,35 @@ import "./banner.scss";
 const Banner = () => {
   return (
     <Container className="banner">
-      <Row className="d-flex justify-between">
-        <Col>
-          <p className="icon">
-            {" "}
-            <Image
-              src="/image/banner/hugeicons_job-link.svg"
-              width={24}
-              height={24}
-              alt="Logo"
-            />
-            Yurtdışı Eğitim Danışmanlık
-          </p>
-          <h2 className="arimo.variable">
-            Hayallerindeki Eğitime <br />
-            <span>Smyrna EDU </span>
-            ile Ulaş!
-          </h2>
-          <p className="banner-text">
-            Gitmek istediğin ülkeyi ve almak istediğin eğitimi seç, fiyat
-            teklifi al ve bizimle iletişime geç! Bütçene uygun dil okulu ve
-            üniversite için anlık fiyatlar alın! Kendinize en uygun programları
-            araştırın!
-          </p>
-        </Col>
-        <Col>
+      <Image
+        src="/image/banner/banner.png"
+        width={1440}
+        height={545}
+        alt="Main-Banner"
+        className="banner-image"
+      />
+      <div className="banner-text-wrapper">
+        <p className="icon">
+          {" "}
           <Image
-            src="/image/banner/banner.png"
-            width={762}
-            height={545}
-            alt="Banner"
+            src="/image/banner/hugeicons_job-link.svg"
+            width={24}
+            height={24}
+            alt="Logo"
           />
-        </Col>
-      </Row>
+          Yurtdışı Eğitim Danışmanlık
+        </p>
+        <h2 className="arimo.variable">
+          Hayallerindeki Eğitime <br />
+          <span>Smyrna EDU </span>
+          ile Ulaş!
+        </h2>
+        <p className="banner-text">
+          Gitmek istediğin ülkeyi ve almak istediğin eğitimi seç, fiyat teklifi
+          al ve bizimle iletişime geç! Bütçene uygun dil okulu ve üniversite
+          için anlık fiyatlar alın! Kendinize en uygun programları araştırın!
+        </p>
+      </div>
     </Container>
   );
 };
