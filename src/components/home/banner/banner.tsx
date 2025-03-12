616b599442982e7fbe0ee290c66403bd3d5a4dc9
enskerti <enskerti@gmail.com>
2025-03-12 17:08:22 +0300

    banner added

--- src/components/home/banner/banner.tsx
+++ src/components/home/banner/banner.tsx
@@ -7,11 +7,11 @@ import data from "@/helpers/data/tr.json";
 import MeetingButton from "./meeting-button";
 
 const Banner = () => {
-  const bannerData =data[0]?.Home || {};
+  const bannerData = data[0]?.Home || {};
   const videoSrc = bannerData["banner-video"];
-  const title = bannerData["banner-title"] ;
-  const description = bannerData["banner-description"] ;
-  const buttonText = bannerData["banner-button"] ;
+  const title = bannerData["banner-title"];
+  const description = bannerData["banner-description"];
+  const buttonText = bannerData["banner-button"];
 
   return (
     <Container fluid className="banner">
@@ -25,16 +25,15 @@ const Banner = () => {
         ></video>
       )}
 
+      <div className="overlay"></div>
+
       <div className="banner-text-wrapper">
-       
-        <h2 className={arimo.variable}>
-          {title}
-        </h2>
+        <h2 className={arimo.variable}>{title}</h2>
         <p className="banner-text">{description}</p>
-        <MeetingButton title={buttonText}/>
+        <MeetingButton title={buttonText} />
       </div>
     </Container>
   );
 };
 
-export default Banner; 
+export default Banner;
