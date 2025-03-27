e5e810d2440576a8e214b8809456761f73efeedd
SumeyyaNacar <sumeyyanacar08@gmail.com>
2025-03-28 00:45:05 +0300

    blog updated

--- src/components/home/blog/blog-card.tsx
+++ src/components/home/blog/blog-card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardBody, CardFooter, CardText } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -8,29 +8,41 @@ interface Props {
 }
 
 interface BlogType {
-    id: number;
-    blogTextTitle: string;
-    image?: string;
-    blogText: string;
-    blogDate: string;
-  }
+  id: number;
+  blogTextTitle: string;
+  image?: string;
+  blogText: string;
+  blogDate: string; // "12.12.2025" formatında
+}
 
-const BlogCard: React.FC<Props> = ({
-  blog,
-}: Props) => {
+const BlogCard: React.FC<Props> = ({ blog }: Props) => {
+  // Tarih formatını dönüştürme fonksiyonu
+  const formatDate = (dateString: string) => {
+    const [day, month, year] = dateString.split(".");
+    const date = new Date(`${year}-${month}-${day}`);
+    const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'short' };
+    return date.toLocaleDateString('en-US', options);
+  };
 
+  // Açıklamayı kısaltma fonksiyonu
+  const truncateDescription = (text: string, length: number) => {
+    return text.length > length ? text.substring(0, length) + "[...]" : text;
+  };
 
   return (
     <Link className="blog-card" href="/blog">
-      <Image src={`/image/${blog.image}`} alt={blog.blogTextTitle} width={250} height={200}/>
+      <Image
+        src={`/image/${blog.image}`}
+        alt={blog.blogTextTitle}
+        width={400}
+        height={300}
+      />
       <Card.Body>
+        <span className="badge bg-secondary">{formatDate(blog.blogDate)}</span> 
         <Card.Title>{blog.blogTextTitle}</Card.Title>
-        <Card.Text>
-          {blog.blogText}
-        </Card.Text>
+        <Card.Text>{truncateDescription(blog.blogText, 20)}</Card.Text> 
       </Card.Body>
     </Link>
-
   );
 };
 
