bd05336cd9fb6b7bd1bb108837b8b853e9d844ce
SumeyyaNacar <sumeyyanacar08@gmail.com>
2025-05-13 15:59:29 +0300

    update on json

---
+++ src/components/pages/common/header/page-navbar-item.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+
+const PageNavbarItem = () => {
+  return (
+    <Nav.Item>
+      <Nav.Link href="/home">Active</Nav.Link>
+    </Nav.Item>
+  );
+};
+
+export default PageNavbarItem;
